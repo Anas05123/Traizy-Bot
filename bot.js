b51f743b6c0f039a72ae7800c38970009ba7f3b6
Minecraft <44584193+anasayari@users.noreply.github.com>
2019-02-02 18:57:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -556,6 +556,28 @@ client.on('message', message => {
    }
 });
 
+client.on('message',async message => {
+  if(message.content === '^unbanall') {
+    var user = message.mentions.users.first();
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**`ADMINISTRATOR`لا توجد لديك صلاحية `**');
+    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+    const guild = message.guild;
+
+  message.guild.fetchBans().then(ba => {
+  ba.forEach(ns => {
+  message.guild.unban(ns);
+  const embed= new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription(`**✅ Has Been Unban For All**`)
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+  message.channel.sendEmbed(embed);
+  guild.owner.send(`سيرفر : ${guild.name}
+  **تم فك الباند عن الجميع بواسطة** : <@${message.author.id}>`) 
+  });
+  });
+  }
+  });
+
 const adminprefix = "-v";
 const devs = ['349616310734553088','335027415619338240'];
 client.on('message', message => {
